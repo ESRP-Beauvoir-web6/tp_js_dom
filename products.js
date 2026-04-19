80397c585d566c39783218534c04d018252fe853
jeremy-di <delta.i.developpement@gmail.com>
2026-04-19 21:47:09 +0200

    first

---
+++ products.js
@@ -0,0 +1,19 @@
+const products = [
+    {
+        name : "Pc Dell 15 pouces",
+        price : 699,
+        picture : "https://www.laptopspirit.fr/wp-content/uploads/new/2021/04/Dell-Inspiron-15-3502-gris-2-scaled.jpg"
+    },
+    {
+        name : "Téléviseur LG 107 cm",
+        price : 599,
+        picture : "https://medias-az-img.ubaldi.com/visuels/54443/42LA660S-43.jpg"
+    },
+    {
+        name : "Citroën Ami",
+        price : 8190,
+        picture : "https://cdn.automobile-propre.com/uploads/2023/07/Citroe%CC%88n-Ami-Buggy-II-1.jpg"
+    },
+]
+
+// Code :
---
+++ users.js
@@ -0,0 +1,45 @@
+const users = [
+    {
+        id : 1,
+        lastName : "Duval",
+        firstName : "Jérome",
+        email : "duval.j@fake.com",
+        login : "djedje",
+        password : "7c4a8d09ca3762af61e59520943dc26494f8941b"
+    },
+    {
+        id : 2,
+        lastName : "Collard",
+        firstName : "Jennifer",
+        email : "cj@fake.com",
+        login : "jenny22110",
+        password : "5b2cfaf08eaf3ecd0ba2b47c0ee8c720257cb690"
+    },
+    {
+        id : 3,
+        lastName : "Caracal",
+        firstName : "Lara",
+        email : "cl@fake.com",
+        login : "crofti",
+        password : "8e117bde25f3881092456b2c2b10cc76e4f55df5"
+    },
+    {
+        id : 4,
+        lastName : "Landers",
+        firstName : "Maxine",
+        email : "lm@fake.com",
+        login : "max",
+        password : "b9e20c0bd5cbc8e1164d3e3d9d3e54993c578a3d"
+    },
+    {
+        id : 5,
+        lastName : "Lerai",
+        firstName : "Frederic",
+        email : "lf@fake.com",
+        login : "fredo",
+        password : "dd8cf2d52e69aafb734e3acd0e4a69e83ed93bc4"
+    },
+]
+
+// Code :
+
